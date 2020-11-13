65aab37e0a6c7ac9851d78f48ae072865ae4da51
TimIsOverpowered <admin@angelthump.com>
2020-11-13 11:51:15 +0000

    sort by newest

--- src/vods.js
+++ src/vods.js
@@ -20,7 +20,7 @@ export default function Vods(props) {
   useEffect(() => {
     document.title = "VODS - Poke";
     const fetchVods = async () => {
-      await fetch("https://poke.gg:2053/vods?$limit=50&$sort[createdAt]=1", {
+      await fetch("https://poke.gg:2053/vods?$limit=50&$sort[createdAt]=-11", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
