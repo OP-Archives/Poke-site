a65161c231241d376d106bafc9d0bf665b985d6e
TimIsOverpowered <admin@angelthump.com>
2022-06-10 22:01:07 -0500

    fix undefined

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ export default function App() {
             path="*"
             element={
               <Parent>
-                <NotFound />
+                <NotFound channel={channel} />
               </Parent>
             }
           />
