7943a453f4cc982f693973b273acdb4c32997c19
TimIsOverpowered <admin@angelthump.com>
2022-06-24 04:36:40 -0500

    {width}x{height} old vods fix

--- src/vods/ChaptersMenu.js
+++ src/vods/ChaptersMenu.js
@@ -20,17 +20,17 @@ export default function Chapters(props) {
     <Box>
       <Tooltip title={vod.chapters[0].name}>
         <IconButton onClick={handleClick}>
-          <img alt="" src={vod.chapters[0].image} style={{ width: "40px", height: "53px" }} />
+          <img alt="" src={getImage(vod.chapters[0].image)} style={{ width: "40px", height: "53px" }} />
         </IconButton>
       </Tooltip>
       <Menu anchorEl={anchorEl} keepMounted open={Boolean(anchorEl)} onClose={handleClose}>
         {vod.chapters.map((data, _) => {
           return (
-            <CustomLink key={data.gameId + data.start} href={`${DEFAULT_VOD}?duration=${data?.start || 0 }`}>
+            <CustomLink key={data.gameId + data.start} href={`${DEFAULT_VOD}?duration=${data?.start || 0}`}>
               <MenuItem>
                 <Box sx={{ display: "flex" }}>
                   <Box sx={{ mr: 1 }}>
-                    <img alt="" src={data.image} style={{ width: "40px", height: "53px" }} />
+                    <img alt="" src={getImage(data.image)} style={{ width: "40px", height: "53px" }} />
                   </Box>
                   <Box sx={{ display: "flex", flexDirection: "column" }}>
                     <Typography color="inherit" variant="body2">{`${data.name}`}</Typography>
@@ -45,3 +45,9 @@ export default function Chapters(props) {
     </Box>
   );
 }
+
+//Support older vods that had {width}x{height} in the link
+const getImage = (link) => {
+  if (!link) return "https://static-cdn.jtvnw.net/ttv-static/404_boxart.jpg";
+  return link.replace("{width}x{height}", "40x53");
+};
