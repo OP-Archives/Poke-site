b2a0101c62f07d7a70281acd17bd3bd9d60ab86a
TimIsOverpowered <admin@angelthump.com>
2024-02-14 06:29:16 -0600

    host change

--- src/contests/client.js
+++ src/contests/client.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 import feathers from "@feathersjs/client";
 
-const socket = io(process.env.REACT_APP_CONTESTS_API);
+const socket = io(process.env.REACT_APP_CONTESTS_SOCKETIO_HOST, { path: process.env.REACT_APP_CONTESTS_SOCKETIO_PATH });
 const client = feathers();
 
 client.configure(feathers.socketio(socket));
